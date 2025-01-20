33cdde21dcc60343dd91fa9b6a0243de0c375c30
Oleksii Sliepov <sliepov@icloud.com>
2025-01-20 20:24:37 +0100

    dodana konfiguracja aktuatora

--- src/main/java/pl/sliepov/egzamin/config/WebConfig.java
+++ src/main/java/pl/sliepov/egzamin/config/WebConfig.java
@@ -12,13 +12,22 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${app.frontend.url}")
     private String frontendUrl;
 
+    @Value("${server.servlet.context-path:/egzaminator-backend}")
+    private String contextPath;
+
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping(contextPath + "/api/**")
                 .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .exposedHeaders("Set-Cookie");
+
+        registry.addMapping(contextPath + "/actuator/**")
+                .allowedOrigins(frontendUrl)
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
